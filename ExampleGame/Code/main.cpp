8322e8972bbc7711655458535fc6dbef8195d2e7
cjharper1 <30304745+cjharper1@users.noreply.github.com>
2019-04-26 20:27:16 -0400

    add sprite moving

--- ExampleGame/Code/main.cpp
+++ ExampleGame/Code/main.cpp
@@ -6,10 +6,8 @@
 int main()
 {
     // CREATE THE MAIN WINDOW.
-    sf::RenderWindow window(
-        sf::VideoMode(1000, 1000),
-        "Example Game",
-        sf::Style::Default);
+    sf::VideoMode desktop_resolution = sf::VideoMode::getDesktopMode();
+    sf::RenderWindow window(desktop_resolution, "Example Game");
 
     // CREATE A GREEN SQUARE.
     sf::Vector2<float> player_character_size = sf::Vector2<float>(10, 10);
@@ -22,10 +20,37 @@ int main()
         sf::Event current_event;
         while (window.pollEvent(current_event))
         {
+            // CHECK IF THE USER CLOSED THE WINDOW.
             if (current_event.type == sf::Event::Closed)
             {
                 window.close();
                 break;
+            }
+            
+            // CHECK IF THE USER PRESSED A KEY.
+            if (current_event.type == sf::Event::KeyPressed)
+            {
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+                {
+                    // Move the character up.
+                    player_character.move(0, -1);
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+                {
+                    // Move the character left.
+                    player_character.move(-1, 0);
+                }
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+                {
+                    // Move the character down.
+                    player_character.move(0, 1);
+                }
+                // HANDLE THE KEY PRESS.
+                if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+                {
+                    // Move the character to the right.
+                    player_character.move(1, 0);
+                }
             }
         }
 
